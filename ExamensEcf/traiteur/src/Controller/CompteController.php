97d2d01ed41464b86c13fda6577561a0547e9624
DASKOV <dav1s@hotmail.fr>
2026-05-12 14:34:52 +0200

    systeme de commande utilisateur

--- ExamensEcf/traiteur/src/Controller/CompteController.php
+++ ExamensEcf/traiteur/src/Controller/CompteController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Commande;
+use App\Form\CommandeType;
+use App\Repository\CommandeRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -12,8 +17,40 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 class CompteController extends AbstractController
 {
     #[Route('/', name: 'app_compte')]
-    public function index(): Response
+    public function index(CommandeRepository $commandeRepository): Response
     {
-        return $this->render('compte/index.html.twig');
+        $commandes = $commandeRepository->findBy(['utilisateur' => $this->getUser()]);
+
+        return $this->render('compte/index.html.twig', [
+            'commandes' => $commandes,
+        ]);
+    }
+
+    #[Route('/commander', name: 'app_commander')]
+    public function commander(Request $request, EntityManagerInterface $em): Response
+    {
+        $commande = new Commande();
+        $form = $this->createForm(CommandeType::class, $commande);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $commande->setNumeroCommande('CMD-' . uniqid());
+            $commande->setDateCommande(new \DateTime());
+            $commande->setStatut('en_attente');
+            $commande->setPretMateriel(false);
+            $commande->setRetourMateriel(false);
+            $commande->setPrixMenu($commande->getMenu()->getPrixParPersonne() * $commande->getNombrePersonne());
+            $commande->setUtilisateur($this->getUser());
+
+            $em->persist($commande);
+            $em->flush();
+
+            $this->addFlash('success', 'Votre commande a été passée avec succès !');
+            return $this->redirectToRoute('app_compte');
+        }
+
+        return $this->render('compte/commander.html.twig', [
+            'form' => $form,
+        ]);
     }
 }
---
+++ ExamensEcf/traiteur/src/Form/CommandeType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Commande;
+use App\Entity\Menu;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CommandeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('date_prestation', DateType::class, [
+                'label' => 'Date de la prestation',
+                'widget' => 'single_text',
+            ])
+            ->add('heure_livraison', TimeType::class, [
+                'label' => 'Heure de livraison',
+                'widget' => 'single_text',
+                'required' => false,
+                'input' => 'string',
+            ])
+            ->add('nombre_personne', IntegerType::class, [
+                'label' => 'Nombre de personnes',
+            ])
+            ->add('menu', EntityType::class, [
+                'class' => Menu::class,
+                'choice_label' => 'titre',
+                'label' => 'Menu choisi',
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Commande::class,
+        ]);
+    }
+}
