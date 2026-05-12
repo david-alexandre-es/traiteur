357a33961ab0be56a540f35a08181164f9f5aa02
DASKOV <dav1s@hotmail.fr>
2026-05-12 14:53:06 +0200

    Espace employé et barre de navigation

--- ExamensEcf/traiteur/src/Controller/EmployeController.php
+++ ExamensEcf/traiteur/src/Controller/EmployeController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Repository\CommandeRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -12,8 +15,20 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 class EmployeController extends AbstractController
 {
     #[Route('/', name: 'app_employe')]
-    public function index(): Response
+    public function index(CommandeRepository $commandeRepository): Response
     {
-        return $this->render('employe/index.html.twig');
+        return $this->render('employe/index.html.twig', [
+            'commandes' => $commandeRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/commandes/{id}/statut', name: 'app_employe_commande_statut', methods: ['POST'])]
+    public function changerStatut(\App\Entity\Commande $commande, Request $request, EntityManagerInterface $em): Response
+    {
+        $statut = $request->request->get('statut');
+        $commande->setStatut($statut);
+        $em->flush();
+        $this->addFlash('success', 'Statut mis à jour !');
+        return $this->redirectToRoute('app_employe');
     }
 }
